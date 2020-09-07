fcafed25814cb871d027d26bf088d5cf6ca1c38e
onur <onur.sabanoglu@nimbo.com.tr>
2020-09-07 18:16:06 +0300

    header changes

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import { Link } from 'gatsby'
+
+import Footer from './footer'
 import headerStyles from './header.module.css'
 import Logo from '../images/logo.png'
 
 const Header = () => {
   return (
     <header className={headerStyles.header}>
-      <Link className={headerStyles.logo} to="/">
-      <img src={Logo} alt="Logo" />
-      </Link>
+      <div className={headerStyles.logoWrapper}>
+        <Link className={headerStyles.logo} to="/">
+        <h1 className={headerStyles.logoTitle}>10ur</h1>
+        </Link>
+      </div>
+      <div className={headerStyles.headerContent}>
+        
+      </div>
+      <Footer />
     </header>
   )
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,21 +2,15 @@ import React from 'react'
 
 
 import Header from './header'
-import Footer from './footer'
+
 import layoutStyles from './layout.module.css'
 
 
 
 const Layout = (props) => {
   return (
-    <div className={layoutStyles.wrapper}>
-      <div className={layoutStyles.container}>
-        <div className={layoutStyles.content}>
-          <Header />
-          {props.children}
-        </div>
-        <Footer />
-      </div>
+    <div className={layoutStyles.container}>
+      <Header />
     </div>
   )
 }
