153311e955908b2613bf0fddb350fc2df6f58b67
onur <onur.sabanoglu@nimbo.com.tr>
2020-09-14 09:44:30 +0300

    seo component added

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,17 +3,21 @@ import React from "react"
 
 import Layout from '../components/layout'
 import SocialLink from '../components/socialLink'
-import Head from '../components/head'
+import SEO from '../components/seo'
+
+
+
 
 
 import '../styles/global.css'
 
 
 
+
 const IndexPage = () => {
   return (
     <Layout>
-      <Head title="Home" />
+      <SEO title="Home" />
       <section className="hero-wrapper">
         <p className="job-title">Programmer of boxes</p>
         <h2 className="hero-heading">Hello <span aria-label="callMe" role="img">🤙</span></h2>
